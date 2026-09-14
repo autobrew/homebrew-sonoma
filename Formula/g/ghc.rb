0ff1cfe4a31aaf8c89116ad83dcf8be0f02d8cb4
Jeroen Ooms <jeroenooms@gmail.com>
2026-09-14 20:53:24 +0200

    Fix formula for homebrew 7
    Remove dependency on gnu-sed for Catalina and older.

--- Formula/g/ghc.rb
+++ Formula/g/ghc.rb
@@ -35,11 +35,6 @@ class Ghc < Formula
   uses_from_macos "libffi"
   uses_from_macos "ncurses"
 
-  # Build uses sed -r option, which is not available in Catalina shipped sed.
-  on_catalina :or_older do
-    depends_on "gnu-sed" => :build
-  end
-
   on_linux do
     on_arm do
       # Work around build failure with Ubuntu 22.04 toolchain (gcc/ld):
